2b022d408e1f8c31fc508da5ffba15de372e31dd
phfr <84philipp@gmail.com>
2026-04-13 16:49:26 +0200

    fix: import os, change path

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    storage_path = "/storage"
+    storage_path = "/mnt/storage"
     exists = os.path.exists(storage_path)
     return {
         "message": "Hello World from asdfhgjhj!",
