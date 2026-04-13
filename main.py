98a84e1f960357db45deebcd2a7ee18fd2cbc0bb
phfr <84philipp@gmail.com>
2026-04-13 16:51:23 +0200

    feat: add healthcheck

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
-import os
 
 app = FastAPI()
 
+@app.get("/healthz")
+def health_check():
+    return {"status": "ok"}
+
+
 @app.get("/")
 def read_root():
-    storage_path = "/mnt/storage"
-    exists = os.path.exists(storage_path)
     return {
         "message": "Hello World from asdfhgjhj!",
-        "storage_mounted": exists,
-        "storage_path": storage_path
     }
